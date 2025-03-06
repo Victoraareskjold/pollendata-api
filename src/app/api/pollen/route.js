5fdc3488c2e7f689b5f93098fe15c368a30be130
Victor Aareskjold <victor.aaareskjold@gmail.com>
2025-03-06 17:18:27 +0100

    initial commit

---
+++ src/app/api/pollen/route.js
@@ -0,0 +1,42 @@
+import puppeteer from "puppeteer";
+
+export async function GET() {
+  try {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+
+    await page.goto("https://pollenvarsel.naaf.no/charts/forecast", {
+      waitUntil: "networkidle2",
+    });
+
+    const pollenData = await page.evaluate(() => {
+      const nextData = window.__NEXT_DATA__;
+      if (nextData && nextData.props && nextData.props.pageProps.data) {
+        return nextData.props.pageProps.data.forecastData;
+      }
+      return null;
+    });
+
+    await browser.close();
+
+    if (!pollenData) {
+      return new Response(
+        JSON.stringify({ success: false, error: "No pollendata found" }),
+        { status: 404, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    return new Response(JSON.stringify({ success: true, data: pollenData }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error fetching pollendata:", error);
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: "Could not fetch pollendata",
+      }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+}
